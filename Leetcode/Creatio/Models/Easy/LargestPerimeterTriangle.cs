3110af4b64e5fd9e54f6f267b6c1b0d7edfe0817
MuradBinyetov <murad_binyetov@mail.ru>
2022-03-23 17:44:04 +0400

    New 2 algorithms completed

---
+++ Leetcode/Creatio/Models/Easy/LargestPerimeterTriangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creatio.Models.Easy
+{
+    public class LargestPerimeterTriangle
+    {
+        public static int LargestPerimeter(int[] nums)
+        {
+
+            return 0;
+        }
+    }
+}
---
+++ Leetcode/Creatio/Models/Easy/ValidMountainArray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creatio.Models.Easy
+{
+    public class ValidMountainArray
+    {
+        public static bool ValidMountainArrayMethod(int[] arr)
+        {
+            if(arr.Length < 3) return false;
+            if (arr[0] >= arr[1]) return false;
+            bool inCreasing = true;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i] == arr[i + 1]) return false;
+
+                if (inCreasing)
+                {
+                    if (arr[i + 1] > arr[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        inCreasing = false;
+                    }
+                }
+                else
+                {
+                    if (arr[i + 1] > arr[i])
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                
+            }
+            if (inCreasing) return false;
+            return true;
+        }
+    }
+}
