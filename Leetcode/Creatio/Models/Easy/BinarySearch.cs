2ef194dca40e2bd2801d4d5cf5072f5ceb5561d1
MuradBinyetov <murad_binyetov@mail.ru>
2022-04-01 18:10:53 +0400

    Search Algorithms solved two ways - Linear search and Binary search

--- Leetcode/Creatio/Models/Easy/BinarySearch.cs
+++ Leetcode/Creatio/Models/Easy/BinarySearch.cs
@@ -7,7 +7,7 @@ namespace Creatio.Models.Easy
 {
     public class BinarySearch
     {
-        public static int Search(int[] nums, int target)
+        public static int Search(int[] nums, int target)  //Linear Search
         {
             if (!nums.Contains(target)) return -1;
             for (int i = 0; i < nums.Length; i++)
@@ -17,5 +17,32 @@ namespace Creatio.Models.Easy
             } 
             return -1;
         }
+
+        public static int BinarySrach2(int[] nums,int target) //BinarySearch  // [3,6,7,9,12]
+        {
+            Array.Sort(nums);
+            int left = 0;  // 0   0  1
+            int right = nums.Length - 1; // 4   1  1
+
+            while (left <= right)
+            {
+                int midIndex = left + (right -left) / 2; //2  0  1
+                if (target == nums[midIndex]) return midIndex;
+
+                else if(nums[midIndex] > target)
+                {
+                    right = midIndex-1;
+                }
+                else
+                {
+                    left = midIndex+1;
+                }
+            }
+
+            return -1;
+
+
+        }
+
     }
 }
