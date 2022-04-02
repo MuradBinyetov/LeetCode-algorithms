301a915b5333982e8e8b25550d32257a69e69457
MuradBinyetov <murad_binyetov@mail.ru>
2022-04-02 14:08:07 +0400

    Selection sort algorithm completed

---
+++ Leetcode/Creatio/Models/Easy/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creatio.Models.Easy
+{
+    public class SelectionSort
+    {
+        public static int[] Sort(int[] arr) //64 25 12 22 11
+        {
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int minIndex = i;
+                for (int j = i+1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+            return arr;
+        }
+    }
+}
