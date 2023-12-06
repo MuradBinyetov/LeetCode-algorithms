bce134ad4eb413febb352a3ee8d7271d99304e0e
murad.binyatov <murad.binyatov@avrora.az>
2023-12-06 17:12:56 +0400

    addBinary algoritm completed

--- Leetcode/Creatio/Models/Easy/AddBinary.cs
+++ Leetcode/Creatio/Models/Easy/AddBinary.cs
@@ -1,16 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Creatio.Models
 {
     public class AddBinary
     {
-        public static string AddBinaryMethod(string a, string b)
+        public static string AddBinaryMethod(string a, string b) //a = "1010", b = "1011"
         {
             //I don't Understand???
-            return "";
+            var stack = new Stack<string>();
+            var resp = "";
+
+            if(a.Length != b.Length)
+            {
+                var length = a.Length > b.Length ? a.Length - b.Length : b.Length - a.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    if(a.Length > b.Length)
+                    {
+                        b = "0" + b;
+                    }
+                    else
+                    {
+                        a = "0" + a;
+                    }
+                }
+            }
+
+            if (a.Length == b.Length)
+            {
+                var reminder = 0;
+                for (int i = a.Length-1; i >=0; i--)
+                { 
+                    var sum =  int.Parse(a[i].ToString()) + int.Parse(b[i].ToString())+reminder;
+                    if(sum == 2)
+                    {
+                        stack.Push("0");
+                        reminder = 1;
+                    }
+                    else if(sum == 3)
+                    {
+                        stack.Push("1");
+                        reminder = 1; 
+                    }
+                    else
+                    {
+                        stack.Push(sum.ToString());
+                        reminder = 0;
+                    }
+                }
+                if(reminder == 1)
+                {
+                    stack.Push("1");
+                }
+            } 
+
+            foreach (var item in stack)
+            {
+                resp += item;
+            }
+            return resp;
         }
     }
 }
---
+++ Leetcode/Creatio/Models/Easy/FindTownJudge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Creatio.Models.Easy
+{
+    public class FindTownJudge
+    {
+        public static int FindJudge(int n, int[][] trust) //[ [1,3], [2,3], [3,1] ]
+        {
+            int response = trust[0][1];
+
+            for (int i = 1; i < trust.Length; i++)
+            {
+                if(trust[i][1] == response)
+                {
+                    continue;
+                }
+                else
+                {
+                    response = -1;
+                }
+            }
+            return response;
+        }
+    }
+}
